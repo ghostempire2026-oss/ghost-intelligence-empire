d077f856b45c2343fbc84a2b7924df86de64f401
ghostempire2026-oss <ghostempire2026@gmail.com>
2026-04-20 12:16:21 -0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,16 +1,10 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+'use client'
+
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const cookieStore = cookies()
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_https://jjdzmmstvjeqycbejiuf.supabase.co,
-    process.env.NEXT_PUBLIC_SUPABASE_,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqZHptbXN0dmplcXljYmVqaXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzEyNzIsImV4cCI6MjA5MjI0NzI3Mn0.YSKDo-UPw2SAAvPNwbe1Z4YbjfsemFMYBb7NhiJyC6Q
-    {
-      cookies: {
-        get: (name) => cookieStore.get(name)?.value,
-      },
-    }
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,sb_publishable_Ch8ohr8C7pStmEpN3sAkWw_3jMqz9t-
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqZHptbXN0dmplcXljYmVqaXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzEyNzIsImV4cCI6MjA5MjI0NzI3Mn0.YSKDo-UPw2SAAvPNwbe1Z4YbjfsemFMYBb7NhiJyC6Q
   )
 }
