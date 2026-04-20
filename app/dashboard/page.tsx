ab2b2426468794e58fdf6c526232e0157a3d2281
ghostempire2026-oss <ghostempire2026@gmail.com>
2026-04-20 15:37:58 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,7 @@
+'use client';
 
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import { DashboardContent } from '@/components/DashboardContent';
 
-export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data: todos } = await supabase.from('todos').select()
-
-  return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
-  )
+export default function DashboardPage() {
+  return <DashboardContent />;
 }
